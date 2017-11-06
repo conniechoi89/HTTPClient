63638623e391e7244614ff603dd3dbd4c6aa64ca
Connie (Moon Hyung) Choi <connie_choi89@hotmail.com>
2017-11-06 00:49:27 +0000

    step 7

--- HTTPClient/https_example.js
+++ HTTPClient/https_example.js
@@ -5,8 +5,8 @@ var https = require('https');
 
 
 var options = {
-  host: 'www.example.org',
-  path: '/'
+  host: 'stream-large-file.herokuapp.com',
+  path: '/give-me-stuff-now'
 };
 
 // called by https when the request is made.
@@ -25,3 +25,5 @@ var callback = function(response) {
   https.request(options, callback).end();
 
   console.log("I've made the request!");
+
+
